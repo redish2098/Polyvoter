fd89b6b14ab07829c705515f8b9678059967a99d
redish <redish361@gmail.com>
2025-02-11 16:29:19 +0000

    remove multiple votes on same submissions

--- bot/cogs/voting.py
+++ bot/cogs/voting.py
@@ -18,15 +18,19 @@ class Voting(commands.Cog):
                 not reaction_event.channel_id == get_setting(Settings.CHANNEL).get()):
             return
 
+        channel = await self.bot.fetch_channel(reaction_event.channel_id)
+        message = await channel.fetch_message(reaction_event.message_id)
         if not get_setting(Settings.VOTING).get():
-            channel = await self.bot.fetch_channel(reaction_event.channel_id)
-            message = await channel.fetch_message(reaction_event.message_id)
             await message.remove_reaction(reaction_event.emoji, reaction_event.member)
             return
 
         if reaction_event.emoji.name in util.reaction_emojis:
             contest_lifecycle.set_vote(reaction_event.user_id, reaction_event.message_id, util.reaction_emojis[reaction_event.emoji.name])
             logger.info(f"user {reaction_event.user_id} voted {util.reaction_emojis[reaction_event.emoji.name]} on {reaction_event.message_id}")
+            for emoji in util.reaction_emojis:
+                if reaction_event.emoji.name == emoji:
+                    continue
+                await message.remove_reaction(emoji, reaction_event.member)
 
 
     @commands.Cog.listener()
